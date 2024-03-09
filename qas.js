9ef66a9721cd86f7000f771a8c36d06a8b70ff60
Psqasim <muhammadqasim0326@gmail.com>
2024-03-09 13:25:51 +0500

    ts file in change

--- qas.js
+++ qas.js
@@ -1,3 +1,9 @@
+// question no 04
 var qoute = "'A person who never made a mistake never tried anything new.'";
 var author = "Albert Einstein";
 console.log("".concat(author, ",once said ").concat(qoute));
+// question 05
+var qoutes = "'A person who never made a mistake never tried anything new.'";
+var famous_person = "Albert Einstein";
+var message = ("".concat(famous_person, ",one said").concat(qoutes));
+console.log(message);
--- qas.ts
+++ qas.ts
@@ -1,3 +1,11 @@
+// question no 04
 let qoute :string="'A person who never made a mistake never tried anything new.'"
 let author:string="Albert Einstein"
 console.log(`${author},once said ${qoute}`);
+
+// question 05
+
+let qoutes :string="'A person who never made a mistake never tried anything new.'"
+let famous_person :string="Albert Einstein"
+let message :string =(`${famous_person},one said${qoutes}`);
+console.log(message);
